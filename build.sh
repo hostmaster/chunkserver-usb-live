a3366caae636aca233550c159adf90a0f8b2c103
Igor Khomyakov <homyakov@gmail.com>
2012-02-07 18:56:12 +0400

    fixed permission

--- build.sh
+++ build.sh
@@ -5,9 +5,3 @@ SRC=/usr/src
 
 make -C $SRC -j 2 buildkernel
 make -C $SRC -j 2 buildworld
-
-make -C $SRC DESTDIR=$USBROOT installkernel
-make -C $SRC DESTDIR=$USBROOT installworld
-
-make -C $SRC/etc DESTDIR=$USBROOT distrib-dirs
-make -C $SRC/etc DESTDIR=$USBROOT distribution
