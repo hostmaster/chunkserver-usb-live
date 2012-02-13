d180f0d00a31fe832dbf2828d5ea2ba22dd12183
Igor Khomyakov <homyakov@gmail.com>
2012-02-13 17:45:08 +0400

    addede TODO

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@
 USBROOT=/USBROOT
 SRC=/usr/src
 
+#rm -rf ${USBROOT}
+
 make -C $SRC -j 2 buildkernel
 make -C $SRC -j 2 buildworld
--- install.sh
+++ install.sh
@@ -8,3 +8,21 @@ make -C $SRC DESTDIR=$USBROOT installworld
 
 make -C $SRC/etc DESTDIR=$USBROOT distrib-dirs
 make -C $SRC/etc DESTDIR=$USBROOT distribution
+
+
+# generate ssh server key
+# install client public keys
+# /etc/rc.conf links 
+
+# install rc.conf
+# install rc.d/home
+# install /boot/loader.conf
+# install screen
+# install bash
+# install vim
+# install moose-chunk-server
+
+# change root password
+# add admin1 user 
+# set admin1 password
+# install admin1 homedir skel
